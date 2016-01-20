0e20e17ce1258b5f7018726801bffc6e5993cbdc
Gerard Roche <gerardroche@users.noreply.github.com>
2016-01-20 03:40:55 +0000

    Use dep. for resizing groups almost equally

--- sublime_ext.py
+++ sublime_ext.py
@@ -437,34 +437,15 @@ class SublimeWindowAPI():
 
     # TODO could implement settings similar to vim window resizing e.g. 'winheight', 'winwidth', 'winfixheight', and 'winfixwidth'
     def resize_groups_almost_equally(self):
+
         """
         Make all groups (almost) equally high and wide, but use 'winheight' and
         'winwidth' for the current window.  Windows with 'winfixheight' set keep
         their height and windows with 'winfixwidth' set keep their width.
         """
 
-        layout = self.window.layout()
-        col_count = len(layout['cols'])
-        row_count = len(layout['rows'])
+        self.window.run_command('resize_groups_almost_equally')
 
-        def equalise(count):
-            size = round(1.0 / (count - 1), 2)
-            vals = [0.0]
-            for i in range(1, count - 1):
-                vals.append(round(size * i, 2))
-            vals.append(1.0)
-            return vals
-
-        if col_count > 2:
-            layout['cols'] = equalise(col_count)
-
-        if row_count > 2:
-            layout['rows'] = equalise(row_count)
-
-        if col_count > 2 or row_count > 2:
-            self.window.set_layout(layout)
-
-    # TODO split_current_view_in_two
     def split_current_view_in_two(self, n=None):
         """
         Split current view in two.  The result is two viewports on
