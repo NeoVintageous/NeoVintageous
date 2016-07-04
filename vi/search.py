0b3a13ea01ccc6e24f491ceb33d29f41c0d6519d
Gerard Roche <gerardroche@users.noreply.github.com>
2016-07-04 17:02:36 +0100

    Better highlighting

--- vi/search.py
+++ vi/search.py
@@ -180,8 +180,13 @@ class BufferSearchBase(sublime_plugin.TextCommand):
         # if State(self.view).settings.vi['hlsearch'] == False:
         #     return
 
-        self.view.add_regions('vi_search', regs, 'vi_search.comment', '', sublime.DRAW_NO_OUTLINE)
-
+        self.view.add_regions(
+            'vi_search',
+            regs,
+            'highlight.vi',
+            '',
+            sublime.DRAW_NO_FILL
+        )
 
 # TODO: Test me.
 class ExactWordBufferSearchBase(BufferSearchBase):
