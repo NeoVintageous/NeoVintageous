309b23150deea5c6323fd54a401b7ce785b5570e
Gerard Roche <gerardroche@users.noreply.github.com>
2017-09-04 16:24:27 +0100

    Internal: Refactor tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
-import sublime_plugin
+from sublime_plugin import reload_plugin
 
 
 # This needs to be done to initialise sublime plugin
 # commands like TextCommand and WindowCommand because
 # sublime only loads .py files from the root package.
-sublime_plugin.reload_plugin('NeoVintageous.tests.commands')
+reload_plugin('NeoVintageous.tests.commands')
