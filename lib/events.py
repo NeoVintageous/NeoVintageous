504e49104f509d8a174438c9c19c5c20ba8b4ef3
Gerard Roche <gerardroche@users.noreply.github.com>
2017-11-02 19:48:12 +0000

    Internal: Documentation

--- lib/events.py
+++ lib/events.py
@@ -184,6 +184,11 @@ class NeoVintageousEvents(EventListener):
         self._on_deactivate_callback_timer = None
 
     def on_query_context(self, view, key, operator, operand, match_all):
+        # Called when determining to trigger a key binding with the given
+        # context key. If the plugin knows how to respond to the context, it
+        # should return either True of False. If the context is unknown, it
+        # should return None.
+
         return _Context(view).query(key, operator, operand, match_all)
 
     def on_query_completions(self, view, prefix, locations):
