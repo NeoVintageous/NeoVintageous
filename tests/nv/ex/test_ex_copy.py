e55f3c88b2da6756c5d2fb8a7a9dedae9fb830ac
Gerard Roche <gerardroche@users.noreply.github.com>
2018-02-23 21:14:01 +0000

    Internal: Refactor ex command tests

--- tests/nv/ex/test_ex_copy.py
+++ tests/nv/ex/test_ex_copy.py
@@ -1,123 +1,103 @@
 from NeoVintageous.tests import unittest
 
 
-class Test_ex_copy_Copying_InNormalMode_SingleLine_DefaultStart(unittest.ViewTestCase):
+class ExCopyTestCaseMixin:
+    def _run_ex_copy(self, command_line):
+        self.view.run_command('ex_copy', {'command_line': command_line})
+
+
+class Test_ex_copy_Copying_InNormalMode_SingleLine_DefaultStart(ExCopyTestCaseMixin, unittest.ViewTestCase):
 
     def test_can_copy_default_line_range(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': 'copy3'})
-
+        self._run_ex_copy('copy3')
         self.assertContent('abc\nxxx\nabc\nxxx\nabc')
 
     def test_can_copy_to_eof(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': 'copy4'})
-
+        self._run_ex_copy('copy4')
         self.assertContent('abc\nxxx\nabc\nabc\nxxx')
 
     def test_can_copy_to_bof(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': 'copy0'})
-
+        self._run_ex_copy('copy0')
         self.assertContent('xxx\nabc\nxxx\nabc\nabc')
 
     def test_can_copy_to_empty_line(self):
         self.write('abc\nxxx\nabc\n\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': 'copy4'})
-
+        self._run_ex_copy('copy4')
         self.assertContent('abc\nxxx\nabc\n\nxxx\nabc')
 
     def test_can_copy_to_same_line(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': 'copy2'})
-
+        self._run_ex_copy('copy2')
         self.assertContent('abc\nxxx\nxxx\nabc\nabc')
 
+    def test_bad_address_does_not_mutate_view(self):
+        self.write('abc')
+        self.select(0)
+        self._run_ex_copy('copy 1,10')
+        self.assertContent('abc')
 
-class Test_ex_copy_Copying_InNormalMode_MultipleLines(unittest.ViewTestCase):
 
-    def setUp(self):
-        super().setUp()
-        self.range = {'left_ref': '.', 'left_offset': 0, 'left_search_offsets': [],
-                      'right_ref': '.', 'right_offset': 1, 'right_search_offsets': []}
+class Test_ex_copy_Copying_InNormalMode_MultipleLines(ExCopyTestCaseMixin, unittest.ViewTestCase):
 
     def test_can_copy_default_line_range(self):
         self.write('abc\nxxx\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': '.,.+1copy4'})
-
+        self._run_ex_copy('.,.+1copy4')
         self.assertContent('abc\nxxx\nxxx\nabc\nxxx\nxxx\nabc')
 
     def test_can_copy_to_eof(self):
         self.write('abc\nxxx\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': '.,.+1copy5'})
-
+        self._run_ex_copy('.,.+1copy5')
         self.assertContent('abc\nxxx\nxxx\nabc\nabc\nxxx\nxxx')
 
     def test_can_copy_to_bof(self):
         self.write('abc\nxxx\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': '.,.+1copy0'})
-
+        self._run_ex_copy('.,.+1copy0')
         self.assertContent('xxx\nxxx\nabc\nxxx\nxxx\nabc\nabc')
 
     def test_can_copy_to_empty_line(self):
         self.write('abc\nxxx\nxxx\nabc\n\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': '.,.+1copy5'})
-
+        self._run_ex_copy('.,.+1copy5')
         self.assertContent('abc\nxxx\nxxx\nabc\n\nxxx\nxxx\nabc')
 
     def test_can_copy_to_same_line(self):
         self.write('abc\nxxx\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': '.,.+1copy2'})
-
+        self._run_ex_copy('.,.+1copy2')
         self.assertContent('abc\nxxx\nxxx\nxxx\nxxx\nabc\nabc')
 
 
-class Test_ex_copy_InNormalMode_CaretPosition(unittest.ViewTestCase):
+class Test_ex_copy_InNormalMode_CaretPosition(ExCopyTestCaseMixin, unittest.ViewTestCase):
 
     def test_can_reposition_caret(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': 'copy 3'})
-
+        self._run_ex_copy('copy 3')
         self.assertContent('abc\nxxx\nabc\nxxx\nabc')
         self.assertSelection(12)
 
 
-class Test_ex_copy_ModeTransition(unittest.ViewTestCase):
+class Test_ex_copy_ModeTransition(ExCopyTestCaseMixin, unittest.ViewTestCase):
 
     def test_from_normal_mode_to_normal_mode(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_copy', {'command_line': 'copy 3'})
-
+        self._run_ex_copy('copy 3')
         self.assertNormalMode()
 
     def test_from_visual_mode_to_normal_mode(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select((4, 5))
-
-        self.view.run_command('ex_copy', {'command_line': 'copy 3'})
-
+        self._run_ex_copy('copy 3')
         self.assertNormalMode()
--- tests/nv/ex/test_ex_move.py
+++ tests/nv/ex/test_ex_move.py
@@ -1,110 +1,90 @@
 from NeoVintageous.tests import unittest
 
 
-class Test_ex_move_Moving_InNormalMode_SingleLine_DefaultStart(unittest.ViewTestCase):
+class ExMoveTestCaseMixin:
+    def _run_ex_move(self, command_line):
+        self.view.run_command('ex_move', {'command_line': command_line})
+
+
+class Test_ex_move_Moving_InNormalMode_SingleLine_DefaultStart(ExMoveTestCaseMixin, unittest.ViewTestCase):
 
     def test_can_move_default_line_range(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move3'})
-
+        self._run_ex_move('move3')
         self.assertContent('abc\nabc\nxxx\nabc')
 
     def test_can_move_to_eof(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move4'})
-
+        self._run_ex_move('move4')
         self.assertContent('abc\nabc\nabc\nxxx')
 
     def test_can_move_to_bof(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move0'})
-
+        self._run_ex_move('move0')
         self.assertContent('xxx\nabc\nabc\nabc')
 
     def test_can_move_to_empty_line(self):
         self.write('abc\nxxx\nabc\n\nabc')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move4'})
-
+        self._run_ex_move('move4')
         self.assertContent('abc\nabc\n\nxxx\nabc')
 
     def test_can_move_to_same_line(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move 2'})
-
+        self._run_ex_move('move 2')
         self.assertContent('abc\nxxx\nabc\nabc')
 
 
-class Test_ex_move_Moveing_InNormalMode_MultipleLines(unittest.ViewTestCase):
+class Test_ex_move_Moveing_InNormalMode_MultipleLines(ExMoveTestCaseMixin, unittest.ViewTestCase):
 
     def test_can_move_default_line_range(self):
         self.write('abc\nxxx\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move4'})
-
+        self._run_ex_move('move4')
         self.assertContent('abc\nxxx\nabc\nxxx\nabc')
 
     def test_can_move_to_eof(self):
         self.write('aaa\nxxx\nxxx\naaa\naaa')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move5'})
-
+        self._run_ex_move('move5')
         self.assertContent('aaa\nxxx\naaa\naaa\nxxx')
 
     def test_can_move_to_bof(self):
         self.write('abc\nxxx\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move0'})
-
+        self._run_ex_move('move0')
         self.assertContent('xxx\nabc\nxxx\nabc\nabc')
 
     def test_can_move_to_empty_line(self):
         self.write('aaa\nxxx\nxxx\naaa\n\naaa')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move5'})
-
+        self._run_ex_move('move5')
         self.assertContent('aaa\nxxx\naaa\n\nxxx\naaa')
 
 
-# TODO: test with multiple selections.
-class Test_ex_move_InNormalMode_CaretPosition(unittest.ViewTestCase):
+class Test_ex_move_InNormalMode_CaretPosition(ExMoveTestCaseMixin, unittest.ViewTestCase):
 
     def test_can_reposition_caret(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move3'})
-
+        self._run_ex_move('move3')
         self.assertSelection(8)
 
 
-class Test_ex_move_ModeTransition(unittest.ViewTestCase):
+class Test_ex_move_ModeTransition(ExMoveTestCaseMixin, unittest.ViewTestCase):
 
     def test_from_normal_mode_to_normal_mode(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select(4)
-
-        self.view.run_command('ex_move', {'command_line': 'move 3'})
-
+        self._run_ex_move('move 3')
         self.assertNormalMode()
 
     def test_from_visual_mode_to_normal_mode(self):
         self.write('abc\nxxx\nabc\nabc')
         self.select((4, 5))
-
-        self.view.run_command('ex_move', {'command_line': 'move3'})
-
+        self._run_ex_move('move3')
         self.assertNormalMode()
