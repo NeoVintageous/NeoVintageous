28698317383393ba0825a89dd2a13da7bc8a4413
Gerard Roche <gerardroche@users.noreply.github.com>
2019-03-20 06:32:51 +0000

    Tests: search

--- tests/nv/vi/test_search.py
+++ tests/nv/vi/test_search.py
@@ -19,7 +19,9 @@ from sublime import LITERAL
 
 from NeoVintageous.tests import unittest
 
+from NeoVintageous.nv.vi.search import find_all_in_range
 from NeoVintageous.nv.vi.search import find_wrapping
+from NeoVintageous.nv.vi.search import reverse_find_wrapping
 from NeoVintageous.nv.vi.search import reverse_search
 from NeoVintageous.nv.vi.search import reverse_search_by_pt
 
@@ -42,6 +44,16 @@ class TestFindWrapping(unittest.ViewTestCase):
         self.assertEqual(self.Region(12, 15), find_wrapping(self.view, 'xxx', 4, self.view.size()))
 
 
+class TestReverseFindWrapping(unittest.ViewTestCase):
+
+    def test_reverse_find_wrapping(self):
+        self.normal('fizz buzz | one two three')
+        self.assertIsNone(reverse_find_wrapping(self.view, 'foo', 3, self.view.size()))
+        self.assertIsNone(reverse_find_wrapping(self.view, 'foo', 15, self.view.size()))
+        self.assertEqual(reverse_find_wrapping(self.view, 'buzz', 10, self.view.size()), self.Region(5, 9))
+        self.assertEqual(reverse_find_wrapping(self.view, 'zz', 10, self.view.size()), self.Region(7, 9))
+
+
 class TestReverseSearchByPt(unittest.ViewTestCase):
 
     def test_found_literal_returns_region(self):
@@ -49,6 +61,14 @@ class TestReverseSearchByPt(unittest.ViewTestCase):
         self.assertEqual(self.Region(0, 1), reverse_search_by_pt(self.view, 'a', start=0, end=3, flags=LITERAL))
         self.assertEqual(self.Region(1, 2), reverse_search_by_pt(self.view, 'b', start=0, end=3, flags=LITERAL))
         self.assertEqual(self.Region(2, 3), reverse_search_by_pt(self.view, 'c', start=0, end=3, flags=LITERAL))
+
+    def test_literal_multiline(self):
+        self.write('abc\ndef\nhij\ndef\nnop')
+        self.assertEqual(reverse_search_by_pt(self.view, 'a', 0, self.view.size(), LITERAL), self.Region(0, 1))
+        self.assertEqual(reverse_search_by_pt(self.view, 'b', 0, self.view.size(), LITERAL), self.Region(1, 2))
+        self.assertEqual(reverse_search_by_pt(self.view, 'd', 0, self.view.size(), LITERAL), self.Region(12, 13))
+        self.assertEqual(reverse_search_by_pt(self.view, 'd', 0, 11, LITERAL), self.Region(4, 5))
+        self.assertEqual(reverse_search_by_pt(self.view, 'a', 1, self.view.size(), LITERAL), None)
 
     def test_literal_not_found_returns_none(self):
         self.write('abc')
@@ -77,6 +97,11 @@ class TestReverseSearchByPt(unittest.ViewTestCase):
         self.assertEqual(None, reverse_search_by_pt(self.view, 'a', start=0, end=2, flags=LITERAL))
         self.assertEqual(None, reverse_search_by_pt(self.view, 'a', start=0, end=1, flags=LITERAL))
         self.assertEqual(None, reverse_search_by_pt(self.view, 'a', start=0, end=0, flags=LITERAL))
+
+    def test_out_of_bounds(self):
+        self.normal('ab|c def')
+        self.assertEqual(reverse_search_by_pt(self.view, 'a', -4, self.view.size()), None)
+        self.assertEqual(reverse_search_by_pt(self.view, 'a', 5, self.view.size() + 1), None)
 
 
 class TestReverseSearch(unittest.ViewTestCase):
@@ -168,3 +193,40 @@ class TestReverseSearch(unittest.ViewTestCase):
         # self.assertEqual(None, reverse_search(self.view, 'a', start=0, end=2, flags=LITERAL))
         # self.assertEqual(None, reverse_search(self.view, 'a', start=0, end=1, flags=LITERAL))
         # self.assertEqual(None, reverse_search(self.view, 'a', start=0, end=0, flags=LITERAL))
+
+    def test_out_of_bounds(self):
+        self.normal('ab|c def')
+        self.assertEqual(reverse_search(self.view, 'a', -4, self.view.size()), None)
+        self.assertEqual(reverse_search(self.view, 'a', 5, self.view.size() + 1), None)
+
+
+class TestFindAllInRange(unittest.ViewTestCase):
+
+    def test_find_one(self):
+        self.write('abc')
+        self.assertEqual(find_all_in_range(self.view, 'a', 0, self.view.size(), LITERAL), [self.Region(0, 1)])
+        self.assertEqual(find_all_in_range(self.view, 'b', 0, self.view.size(), LITERAL), [self.Region(1, 2)])
+        self.assertEqual(find_all_in_range(self.view, 'c', 0, self.view.size(), LITERAL), [self.Region(2, 3)])
+        self.assertEqual(find_all_in_range(self.view, 'x', 0, self.view.size(), LITERAL), [])
+        self.assertEqual(find_all_in_range(self.view, 'a', 1, self.view.size(), LITERAL), [])
+        self.assertEqual(find_all_in_range(self.view, 'b', 1, self.view.size(), LITERAL), [self.Region(1, 2)])
+        self.assertEqual(find_all_in_range(self.view, 'b', 2, self.view.size(), LITERAL), [])
+        self.assertEqual(find_all_in_range(self.view, 'c', 2, self.view.size(), LITERAL), [self.Region(2, 3)])
+
+    def test_find_many(self):
+        self.write('abc abc abc')
+        self.assertEqual(find_all_in_range(self.view, 'x', 0, self.view.size()), [])
+        self.assertEqual(find_all_in_range(self.view, 'a', 0, self.view.size()), [
+            self.Region(0, 1),
+            self.Region(4, 5),
+            self.Region(8, 9)
+        ])
+        self.assertEqual(find_all_in_range(self.view, 'c', 0, self.view.size()), [
+            self.Region(2, 3),
+            self.Region(6, 7),
+            self.Region(10, 11)
+        ])
+        self.assertEqual(find_all_in_range(self.view, 'b', 0, 7), [
+            self.Region(1, 2),
+            self.Region(5, 6)
+        ])
