dac4483f5357dc82e28bfac6680133063705220f
Gerard Roche <gerardroche@users.noreply.github.com>
2019-04-02 19:47:30 +0100

    Tests: W

--- tests/functional/test__big_w.py
+++ tests/functional/test__big_w.py
@@ -20,7 +20,7 @@ from NeoVintageous.tests import unittest
 
 class Test_W(unittest.FunctionalTestCase):
 
-    def test_W(self):
+    def test_n(self):
         self.eq('x one |two. three', 'n_W', 'x one two. |three')
         self.eq('x |one, t.- three', 'n_2W', 'x one, t.- |three')
         self.eq('x |on.e t.wo', 'n_2W', 'x on.e t.w|o')
@@ -29,7 +29,7 @@ class Test_W(unittest.FunctionalTestCase):
         self.eq('|on.e t.wo', 'n_2W', 'on.e t.w|o')
         self.eq('|one, t.- a&^$x four x', 'n_3W', 'one, t.- a&^$x |four x')
 
-    def test_v_w(self):
+    def test_v(self):
         self.eq('one |t=- three', 'v_W', 'one |t=- t|hree')
         self.eq('x |one two x', 'v_W', 'x |one t|wo x')
         self.eq('x |one_ two$ three x', 'v_2W', 'x |one_ two$ t|hree x')
@@ -42,3 +42,9 @@ class Test_W(unittest.FunctionalTestCase):
         self.eq('r_|one t|wo', 'v_W', 'r_one |t|wo')
         self.eq('r_|one |two', 'v_W', 'one| t|wo')
         self.eq('fi|zz', 'v_W', 'fi|zz|')
+
+    def test_d(self):
+        self.eq('one |two three', 'dW', 'one |three')
+        self.eq('one |t.$£;,wo three', 'dW', 'one |three')
+        self.eq('1\n|\n2\n3', 'dW', '1\n|2\n3')
+        self.eq('1\n|\n\n2\n3', 'dW', '1\n|\n2\n3')
