f1e5a78e794c240ea56daa18e8b43bb7bb534f4d
Gerard Roche <gerardroche@users.noreply.github.com>
2019-04-02 20:03:36 +0100

    Tests: B and b

--- tests/functional/test__big_b.py
+++ tests/functional/test__big_b.py
@@ -22,7 +22,21 @@ class Test_B(unittest.FunctionalTestCase):
 
     def test_n(self):
         self.eq('fizz b.^%4u|zz', 'n_B', 'fizz |b.^%4uzz')
+        self.eq('ab|c', 'n_B', '|abc')
+        self.eq('abc a|bc', 'n_B', 'abc |abc')
+        self.eq('abc |a', 'n_B', '|abc a')
+        self.eq('abc |abc', 'n_B', '|abc abc')
 
     def test_v(self):
         self.eq('fizz b.^%|4uz|z', 'v_B', 'r_fizz |b.^%4|uzz')
         self.eq('r_fizz b.^%|4u|zz', 'v_B', 'r_fizz |b.^%4u|zz')
+        self.eq('r_ab|c', 'v_B', 'r_|abc|')
+        self.eq('abc |abc', 'v_B', 'r_|abc a|bc')
+        self.eq('abc |a|', 'v_B', 'r_|abc a|')
+        self.eq('abc |abc|', 'v_B', 'abc |a|bc')
+        self.eq('|abc abc|', 'v_B', '|abc a|bc')
+
+    def test_d(self):
+        self.eq('fi|zz', 'dB', '|zz')
+        self.eq('fizz bu|zz', 'dB', 'fizz |zz')
+        self.eq('fizz bu.,!;|zz', 'dB', 'fizz |zz')
--- tests/functional/test_b.py
+++ tests/functional/test_b.py
@@ -20,21 +20,14 @@ from NeoVintageous.tests import unittest
 
 class Test_b(unittest.FunctionalTestCase):
 
-    def test_b(self):
-        self.eq('ab|c', 'b', 'r_N_|ab|c')
-        self.eq('abc\n|', 'b', 'r_N_|abc\n|')
-        self.eq('one two thr|ee', 'b', 'r_N_one two |thr|ee')
-        self.eq('one two |three', 'b', 'r_N_one |two |three')
-        self.eq('one two three fo|ur', '3b', 'r_N_one |two three fo|ur')
-
-    def test_n_b(self):
+    def test_n(self):
         self.eq('ab|c', 'n_b', '|abc')
         self.eq('abc\n|', 'n_b', '|abc\n')
         self.eq('one two thr|ee', 'n_b', 'one two |three')
         self.eq('one two |three', 'n_b', 'one |two three')
         self.eq('one two three fo|ur', 'n_3b', 'one |two three four')
 
-    def test_v_b(self):
+    def test_v(self):
         self.visual('one two three four f|ive six seven ei|ght')
         self.feed('v_b')
         self.assertVisual('one two three four f|ive six seven e|ight')
@@ -47,10 +40,23 @@ class Test_b(unittest.FunctionalTestCase):
         self.feed('v_b')
         self.assertRVisual('|one two three four fi|ve six seven eight')
 
-    def test_vline_b(self):
+    def test_V(self):
         self.eq('x\n|one\n|y\n', 'l_b')
         self.eq('x\n|one\ntwo\n|y\n', 'l_b')
 
-    def test_vblock_b(self):
+    def test_b(self):
         self.eq('one t|w|o\none t|w|o', 'b_b', 'r_one |tw|o\none |tw|o')
         self.eq('one two t|hr|ee\none two t|hr|ee', 'b_2b', 'r_one |two th|ree\none |two th|ree')
+        self.eq('r_fizz bu|zz|\nfizz bu|zz|\n', 'b_b', 'r_fizz |buzz|\nfizz |buzz|\n')
+        self.eq('fi|zz buz|z\nfi|zz buz|z\n', 'b_b', 'fi|zz b|uzz\nfi|zz b|uzz\n')
+        self.eq('fizz b|uz|z\nfizz b|uz|z\n', 'b_b', 'r_fizz |bu|zz\nfizz |bu|zz\n')
+
+    def test_c(self):
+        self.eq('x fizz|buzz x', 'cb', 'i_x |buzz x')
+        self.eq('ab|c', 'cb', 'i_|c')
+        self.eq('abc\n|', 'cb', 'i_|\n')
+        self.eq('one two thr|ee', 'cb', 'i_one two |ee')
+        self.eq('one two |three', 'cb', 'i_one | three')
+        self.eq('one two three fo|ur', '3cb', 'i_one |ur')
+        self.eq('one two |three', 'cb', 'i_one | three')
+        self.eq('one two three fo|ur', '3cb', 'i_one |ur')
