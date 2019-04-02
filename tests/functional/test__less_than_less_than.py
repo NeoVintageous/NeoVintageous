f18ff82f213375d55430631cda2b6ba222024e83
Gerard Roche <gerardroche@users.noreply.github.com>
2019-04-02 20:02:15 +0100

    Tests: <<

---
+++ tests/functional/test__less_than_less_than.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2018 The NeoVintageous Team (NeoVintageous).
+#
+# This file is part of NeoVintageous.
+#
+# NeoVintageous is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# NeoVintageous is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NeoVintageous.  If not, see <https://www.gnu.org/licenses/>.
+
+from NeoVintageous.tests import unittest
+
+
+class Test_less_than_less_than(unittest.FunctionalTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.settings().set('translate_tabs_to_spaces', True)
+        self.settings().set('tab_size', 4)
+
+    def test_n(self):
+        self.eq('    |fizz', 'n_<<', '|fizz')
+        self.eq('|        abc', 'n_<<', '    |abc')
+        self.eq('|        abc', '1<<', '    |abc')
+        self.eq('|    abc\n    abc', '2<<', '|abc\nabc')
+        self.eq('|    abc\n    abc\n    abc', '3<<', '|abc\nabc\nabc')
