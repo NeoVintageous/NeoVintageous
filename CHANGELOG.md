9abf2b04654d76653f824ab9169a9d714d048789
Gerard Roche <gerardroche@users.noreply.github.com>
2019-06-14 17:42:31 +0100

    Fix #583 :move cursor position
    Fixes #584 :copy cursor position

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,13 @@
 
 All notable changes are documented in this file using the [Keep a CHANGELOG](http://keepachangelog.com/) principles.
 
+## 1.16.2 - Unreleased
+
+### Fixed
+
+* Fixed [#583](https://github.com/NeoVintageous/NeoVintageous/issues/583): `:move` has inconsistent cursor behaviour
+* Fixed [#584](https://github.com/NeoVintageous/NeoVintageous/issues/584): `:copy` to EOF adds too many newlines
+
 ## 1.16.1 - 2019-06-13
 
 ### Fixed
--- tests/functional/test__ex_move.py
+++ tests/functional/test__ex_move.py
@@ -20,14 +20,36 @@ from NeoVintageous.tests import unittest
 
 class Test_ex_move(unittest.FunctionalTestCase):
 
-    def test_move(self):
+    def test_n(self):
+        self.eq('a\n|b\nc', ':move 0', '|b\na\nc')
+        self.eq('a\n|b\nc', ':move 1', '|a\nb\nc')
+        self.eq('a\n|b\nc', ':move 2', 'a\n|b\nc')
+        self.eq('a\n|b\nc', ':move 3', 'a\nc\n|b')
+
+        self.eq('a\n|b\nc', ':move2', 'a\n|b\nc')
+
+        self.eq('a\n|b\nc', ':m 2', 'a\n|b\nc')
+        self.eq('a\n|b\nc', ':m2', 'a\n|b\nc')
+
         self.eq('a\n|b\nc\n', ':move 0', '|b\na\nc\n')
         self.eq('a\n|b\nc\n', ':move 1', '|a\nb\nc\n')
         self.eq('a\n|b\nc\n', ':move 2', 'a\n|b\nc\n')
-        self.eq('a\n|b\nc\n', ':move 3', 'a\nc\n|\nb')
-        self.eq('aaa\n|xxx\nxxx\naaa\n\naaa', ':move5', 'aaa\nxxx\naaa\n\nxx|x\naaa')
-        self.eq('aaa\n|xxx\nxxx\naaa\naaa', ':move5', 'aaa\nxxx\naaa\naaa\n|xxx')
-        self.eq('abc\n|xxx\nabc\n\nabc', ':move4', 'abc\nabc\n\nxx|x\nabc')
+        self.eq('a\n|b\nc\n', ':move 3', 'a\nc\n|b\n')
+
+        self.eq('a\nb\nc\n|x', ':move 0', '|x\na\nb\nc\n')
+        self.eq('a\nb\nc\n|x', ':move 1', 'a\n|x\nb\nc\n')
+        self.eq('a\nb\nc\n|x', ':move 2', 'a\nb\n|x\nc\n')
+
+        self.eq('a\nb\nc\n|x\n', ':move 0', '|x\na\nb\nc\n')
+        self.eq('a\nb\nc\n|x\n', ':move 1', 'a\n|x\nb\nc\n')
+        self.eq('a\nb\nc\n|x\n', ':move 2', 'a\nb\n|x\nc\n')
+
+        self.eq('a\nb\n|x\nc', ':move 0', '|x\na\nb\nc')
+        self.eq('a\nb\n|x\nc', ':move 1', 'a\n|x\nb\nc')
+
+        self.eq('a\nb\n|x\nc\n', ':move 0', '|x\na\nb\nc\n')
+        self.eq('a\nb\n|x\nc\n', ':move 1', 'a\n|x\nb\nc\n')
+
         self.eq('abc\n|xxx\nabc\nabc', ':move 2', 'abc\n|xxx\nabc\nabc')
         self.eq('abc\n|xxx\nabc\nabc', ':move 3', 'abc\nabc\n|xxx\nabc')
         self.eq('abc\n|xxx\nabc\nabc', ':move0', '|xxx\nabc\nabc\nabc')
@@ -36,10 +58,26 @@ class Test_ex_move(unittest.FunctionalTestCase):
         self.eq('abc\n|xxx\nxxx\nabc\nabc', ':move0', '|xxx\nabc\nxxx\nabc\nabc')
         self.eq('abc\n|xxx\nxxx\nabc\nabc', ':move4', 'abc\nxxx\nabc\n|xxx\nabc')
 
-    def test_v_move(self):
+        self.eq('111\n|222\n333\n444\n555', ':move5', '111\n333\n444\n555\n|222')
+        self.eq('111\n|222\n333\n444\n\n555', ':move5', '111\n333\n444\n\n|222\n555')
+        self.eq('111\n|222\n333\n\n444', ':move4', '111\n333\n\n|222\n444')
+
+        self.eq('1\n|buzz\nfizz\n4\n', ':move .+1', '1\nfizz\n|buzz\n4\n')
+        self.eq('1\n|buzz\nfizzer\n4\n', ':move .+1', '1\nfizzer\n|buzz\n4\n')
+        self.eq('1\n|buzzer\nfizz\n4\n', ':move .+1', '1\nfizz\n|buzzer\n4\n')
+
+        self.eq('1\nbuzz\n|fizz\n4\n', ':move .-2', '1\n|fizz\nbuzz\n4\n')
+        self.eq('1\nbuzz\n|fizzer\n4\n', ':move .-2', '1\n|fizzer\nbuzz\n4\n')
+        self.eq('1\nbuzzer\n|fizz\n4\n', ':move .-2', '1\n|fizz\nbuzzer\n4\n')
+
+    def test_v(self):
+        self.eq('abc\n|x|xx\nabc\nabc', ":'<,'>move 3", 'n_abc\nabc\n|xxx\nabc')
         self.eq('abc\n|x|xx\nabc\nabc', ":'<,'>move3", 'n_abc\nabc\n|xxx\nabc')
+
+        self.eq('abc\n|x|xx\nabc\nabc', ":'<,'>m 3", 'n_abc\nabc\n|xxx\nabc')
+        self.eq('abc\n|x|xx\nabc\nabc', ":'<,'>m3", 'n_abc\nabc\n|xxx\nabc')
 
     @unittest.mock_status_message()
     def test_cannot_move_lines_into_themselves(self):
-        self.eq('|1\n2\n3\n|4', ':\'<,\'>move 2', 'n_1\n2\n3\n|4')
+        self.eq('|1\n2\n3\n|4', ':\'<,\'>move 2', 'v_|1\n2\n3\n|4')
         self.assertStatusMessage('E134: Move lines into themselves')
