66739c18331bff19f1c9b71d3ab64b9afadff9df
Gerard Roche <gerardroche@users.noreply.github.com>
2019-07-18 17:51:54 +0100

    Doc: Update

--- nv/rc.py
+++ nv/rc.py
@@ -41,7 +41,7 @@ def open(window):
 
     if not os.path.exists(file):
         with builtins.open(file, 'w', encoding='utf-8') as f:
-            f.write('" Type :h neovintageousrc for help.\n')
+            f.write('" Type :help nv for help.\n')
 
     window.open_file(file)
 
