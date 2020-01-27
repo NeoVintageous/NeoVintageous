1b0a519e3265a8ccda67ae30a4745eb27e606927
Gerard Roche <gerardroche@users.noreply.github.com>
2020-01-27 23:21:10 +0000

    Fix sessions JSON dict key STR to int conversions
    > Note Keys in key/value pairs of JSON are always of the type str. When a
    > dictionary is converted into JSON, all the keys of the dictionary are
    > coerced to strings. As a result of this, if a dictionary is converted
    > into JSON and then back into a dictionary, the dictionary may not equal
    > the original one. That is, loads(dumps(x)) != x if x has non-string
    > keys. https://docs.python.org/3.4/library/json.html
    
    Re 1f7470813f349ed2707c079fde70b342f0d63f1c

--- nv/session.py
+++ nv/session.py
@@ -11,12 +11,20 @@ def _session_file() -> str:
     return os.path.join(os.path.dirname(packages_path()), 'Local', 'nvinfo')
 
 
+def _json_object_hook_dict_str_key_to_int(x):
+    if isinstance(x, dict):
+        return {int(k) if k.isdigit() else k: v for k, v in x.items()}
+
+    return x
+
+
 def load_session() -> None:
     try:
+
         with open(_session_file(), 'r', encoding='utf=8', errors='replace') as f:
             content = f.read()
             if content.strip():
-                session = json.loads(content)
+                session = json.loads(content, object_hook=_json_object_hook_dict_str_key_to_int)
                 if session:
                     for k, v in session.items():
                         # TODO The history module needs to be refactored to
