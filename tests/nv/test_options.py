74738143f7e6d4e011dac8c1a400823ae4e13bea
Gerard Roche <gerardroche@users.noreply.github.com>
2020-02-04 19:52:40 +0000

    Tests: options

--- tests/nv/test_options.py
+++ tests/nv/test_options.py
@@ -21,22 +21,28 @@ from NeoVintageous.tests import unittest
 
 from NeoVintageous.nv.options import BooleanIsVisibleOption
 from NeoVintageous.nv.options import BooleanViewOption
+from NeoVintageous.nv.options import NumberOption
 from NeoVintageous.nv.options import Option
 from NeoVintageous.nv.options import StringOption
+from NeoVintageous.nv.options import get_option
 from NeoVintageous.nv.options import get_option_completions
+from NeoVintageous.nv.options import set_option
+from NeoVintageous.nv.options import toggle_option
+from NeoVintageous.nv.options import window_visible_option
 
 
 class TestOption(unittest.ViewTestCase):
 
-    def test_set(self):
-        option = Option('belloff', '')
-        option.set(self.view, '')
+    def test_option(self):
+        option = Option('fizzbuzz', '')
         self.assertEqual(option.get(self.view), '')
+        option.set(self.view, 'x')
+        self.assertEqual(option.get(self.view), 'x')
         option.set(self.view, 'all')
         self.assertEqual(option.get(self.view), 'all')
         option.set(self.view, '')
         self.assertEqual(option.get(self.view), '')
-        self.assertNotSetting('belloff')
+        self.assertNotSetting('fizzbuzz')
 
 
 class TestStringOption(unittest.ViewTestCase):
@@ -61,6 +67,19 @@ class TestStringOption(unittest.ViewTestCase):
         for invalid_value in ('foobar', '', None, False):
             with self.assertRaisesRegex(ValueError, 'invalid argument'):
                 option.set(self.view, invalid_value)
+
+
+class TestNumberOption(unittest.ViewTestCase):
+
+    def test_number(self):
+        option = NumberOption('fizz', 0)
+        self.assertEqual(option.get(self.view), 0)
+        option.set(self.view, 3)
+        self.assertEqual(option.get(self.view), 3)
+        option.set(self.view, '3')
+        self.assertEqual(option.get(self.view), 3)
+        option.set(self.view, '42')
+        self.assertEqual(option.get(self.view), 42)
 
 
 class TestBooleanViewOption(unittest.ViewTestCase):
@@ -127,7 +146,7 @@ class TestBooleanIsVisibleOption(unittest.ViewTestCase):
         self.view.window().set_status_bar_visible(self.is_status_bar_visible)
         super().tearDown()
 
-    def test_set(self):
+    def test_is_visible(self):
         toggles = [
             'minimap',
             'sidebar',
@@ -144,12 +163,53 @@ class TestBooleanIsVisibleOption(unittest.ViewTestCase):
             option.set(self.view, True)
             self.assertEqual(option.get(self.view), True)
             self.assertOption(name.replace('_', ''), True)
+
             option.set(self.view, False)
             self.assertEqual(option.get(self.view), False)
             self.assertOption(name.replace('_', ''), False)
+
             option.set(self.view, True)
             self.assertEqual(option.get(self.view), True)
             self.assertOption(name.replace('_', ''), True)
+
+            # Test toggle (used by for example Unimpaired).
+            window_visible_option(self.view, name)
+            self.assertEqual(option.get(self.view), False)
+            self.assertOption(name.replace('_', ''), False)
+
+            # Test toggle (used by for example Unimpaired).
+            window_visible_option(self.view, name)
+            self.assertEqual(option.get(self.view), True)
+            self.assertOption(name.replace('_', ''), True)
+
+
+class TestGetSetOptions(unittest.ViewTestCase):
+
+    def test_get(self):
+        set_option(self.view, 'hlsearch', False)
+        self.assertEqual(get_option(self.view, 'hlsearch'), False)
+        self.assertEqual(get_option(self.view, 'hls'), False)
+        set_option(self.view, 'hlsearch', True)
+        self.assertEqual(get_option(self.view, 'hlsearch'), True)
+        self.assertEqual(get_option(self.view, 'hls'), True)
+        set_option(self.view, 'nohlsearch')
+        self.assertEqual(get_option(self.view, 'hlsearch'), False)
+        set_option(self.view, 'hlsearch')
+        self.assertEqual(get_option(self.view, 'hlsearch'), True)
+
+    def test_toggle(self):
+        set_option(self.view, 'hlsearch', True)
+        toggle_option(self.view, 'hlsearch')
+        self.assertEqual(get_option(self.view, 'hlsearch'), False)
+        toggle_option(self.view, 'hlsearch')
+        self.assertEqual(get_option(self.view, 'hlsearch'), True)
+
+    def test_set_unknown_raises_exception(self):
+        with self.assertRaisesRegex(KeyError, 'foobar'):
+            set_option(self.view, 'foobar', False)
+
+        with self.assertRaisesRegex(KeyError, 'nofoobar'):
+            set_option(self.view, 'nofoobar', False)
 
 
 class TestCompletions(unittest.TestCase):
