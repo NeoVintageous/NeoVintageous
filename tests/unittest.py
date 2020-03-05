956874efaa031e899c39cddfaef83f39356db478
Gerard Roche <gerardroche@users.noreply.github.com>
2020-03-05 13:06:37 +0000

    Tests: .

--- tests/unittest.py
+++ tests/unittest.py
@@ -1287,7 +1287,7 @@ _SEQ2CMD = {
     '*':            {'command': '_vi_star'},  # noqa: E241
     ',':            {'command': '_nv_feed_key'},  # noqa: E241
     '-':            {'command': '_vi_minus'},  # noqa: E241
-    '.':            {'command': '_vi_dot'},  # noqa: E241
+    '.':            {'command': '_nv_feed_key'},  # noqa: E241
     '/abc':         {'command': '_vi_slash_impl', 'args': {'search_string': 'abc'}},  # noqa: E241
     '/x':           {'command': '_vi_slash_impl', 'args': {'search_string': 'x'}},  # noqa: E241
     '0':            {'command': '_vi_zero'},  # noqa: E241
