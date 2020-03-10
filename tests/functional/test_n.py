278371b05f7315353dfc0c1250fa2de529d69e0c
Gerard Roche <gerardroche@users.noreply.github.com>
2020-03-10 08:17:41 +0000

    Tests: n

--- tests/functional/test_n.py
+++ tests/functional/test_n.py
@@ -24,7 +24,7 @@ class Test_n(unittest.FunctionalTestCase):
         super().setUp()
         self.set_option('wrapscan', True)
 
-    def test_n_repeat_star_forward(self):
+    def test_n_repeat_star(self):
         self.normal('foo\n|abc\nbar\nabc\nmoo\nabc\nend')
         self.feed('n_*')
         self.feed('n_n')
@@ -37,17 +37,17 @@ class Test_n(unittest.FunctionalTestCase):
         self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\n|abc|\nend')
         self.assertSearchCurrent('foo\nabc\nbar\n|abc|\nmoo\nabc\nend')
 
-    def test_n_repeat_star_forward_no_partial(self):
-        self.normal('foo\n|abc\nbar\nabc\nmoo\nabcxend')
+    def test_n_repeat_star_should_only_match_whole_words(self):
+        self.normal('foo\n|abc\nbar\nabc\nmoo\nabcxend\nabc\n')
         self.feed('n_*')
         self.feed('n_n')
-        self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\nabcxend')
-        self.assertSearchCurrent('foo\n|abc|\nbar\nabc\nmoo\nabcxend')
+        self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\nabcxend\n|abc|\n')
+        self.assertSearchCurrent('foo\nabc\nbar\nabc\nmoo\nabcxend\n|abc|\n')
         self.feed('n_n')
-        self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\nabcxend')
-        self.assertSearchCurrent('foo\nabc\nbar\n|abc|\nmoo\nabcxend')
+        self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\nabcxend\n|abc|\n')
+        self.assertSearchCurrent('foo\n|abc|\nbar\nabc\nmoo\nabcxend\nabc\n')
 
-    def test_n_repeat_star_forward_should_not_use_smartcase(self):
+    def test_n_repeat_star_should_use_ignorecase_option_but_not_smartcase_option(self):
         self.normal('x|Xx\nXXX\nxXx\nXxX\n')
         self.set_option('ignorecase', True)
         self.set_option('smartcase', True)
@@ -58,14 +58,20 @@ class Test_n(unittest.FunctionalTestCase):
         self.assertSearch('|xXx|\n|XXX|\n|xXx|\n|XxX|\n')
         self.assertSearchCurrent('xXx\nXXX\n|xXx|\nXxX\n')
 
-    def test_n_repeat_octothorp_forward(self):
-        self.normal('foo\n|abc\nbar\nabc\nmoo\nabc\nend')
+    def test_n_repeat_octothorp(self):
+        self.normal('foo\nabc\nbar\nabc\nmoo\n|abc\nend')
         self.feed('n_#')
+        self.feed('n_n')
+        self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\n|abc|\nend')
+        self.assertSearchCurrent('foo\n|abc|\nbar\nabc\nmoo\nabc\nend')
+        self.feed('n_n')
+        self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\n|abc|\nend')
+        self.assertSearchCurrent('foo\nabc\nbar\nabc\nmoo\n|abc|\nend')
         self.feed('n_n')
         self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\n|abc|\nend')
         self.assertSearchCurrent('foo\nabc\nbar\n|abc|\nmoo\nabc\nend')
 
-    def test_n_repeat_slash_forward(self):
+    def test_n_repeat_slash(self):
         self.normal('|foo\nabc\nbar\nabc\nmoo\nabc\nend')
         self.feed('n_/abc')
         self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\n|abc|\nend')
@@ -80,7 +86,32 @@ class Test_n(unittest.FunctionalTestCase):
         self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\n|abc|\nend')
         self.assertSearchCurrent('foo\n|abc|\nbar\nabc\nmoo\nabc\nend')
 
-    def test_n_repeat_slash_partial(self):
+    def test_n_repeat_slash_should_use_ignorecase_and_smartcase(self):
+        self.normal('abc\na|Bc\nABC\naBc\nabc\naBc\nABC\naBc\n')
+        self.set_option('ignorecase', True)
+        self.set_option('smartcase', True)
+        self.feed('n_/aBc')
+        self.feed('n_n')
+        self.assertSearch('abc\n|aBc|\nABC\n|aBc|\nabc\n|aBc|\nABC\n|aBc|\n')
+        self.assertSearchCurrent('abc\naBc\nABC\naBc\nabc\n|aBc|\nABC\naBc\n')
+        self.feed('n_n')
+        self.assertSearch('abc\n|aBc|\nABC\n|aBc|\nabc\n|aBc|\nABC\n|aBc|\n')
+        self.assertSearchCurrent('abc\naBc\nABC\naBc\nabc\naBc\nABC\n|aBc|\n')
+        self.normal('abc\na|Bc\nABC\naBc\nabc\naBc\nABC\naBc\n')
+        self.set_option('ignorecase', True)
+        self.set_option('smartcase', False)
+        self.feed('n_/aBc')
+        self.feed('n_n')
+        self.assertSearch('|abc|\n|aBc|\n|ABC|\n|aBc|\n|abc|\n|aBc|\n|ABC|\n|aBc|\n')
+        self.assertSearchCurrent('abc\naBc\nABC\n|aBc|\nabc\naBc\nABC\naBc\n')
+        self.feed('n_n')
+        self.assertSearch('|abc|\n|aBc|\n|ABC|\n|aBc|\n|abc|\n|aBc|\n|ABC|\n|aBc|\n')
+        self.assertSearchCurrent('abc\naBc\nABC\naBc\n|abc|\naBc\nABC\naBc\n')
+        self.feed('n_2n')
+        self.assertSearch('|abc|\n|aBc|\n|ABC|\n|aBc|\n|abc|\n|aBc|\n|ABC|\n|aBc|\n')
+        self.assertSearchCurrent('abc\naBc\nABC\naBc\nabc\naBc\n|ABC|\naBc\n')
+
+    def test_n_repeat_slash_only_matches_whole_words(self):
         self.normal('|foo\nabc\nbar\nabcxmoo\nabc\nend')
         self.feed('n_/abc')
         self.assertSearch('foo\n|abc|\nbar\n|abc|xmoo\n|abc|\nend')
@@ -95,8 +126,8 @@ class Test_n(unittest.FunctionalTestCase):
         self.assertSearch('foo\n|abc|\nbar\n|abc|xmoo\n|abc|\nend')
         self.assertSearchCurrent('foo\n|abc|\nbar\nabcxmoo\nabc\nend')
 
-    def test_n_repeat_question_mark_forward(self):
-        self.normal('|foo\nabc\nbar\nabc\nmoo\nabc\nend')
+    def test_n_repeat_question_mark(self):
+        self.normal('foo\nabc\nbar\nabc\nmoo\nabc\ne|nd')
         self.feed('n_?abc')
         self.feed('n_n')
         self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\n|abc|\nend')
@@ -104,8 +135,11 @@ class Test_n(unittest.FunctionalTestCase):
         self.feed('n_n')
         self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\n|abc|\nend')
         self.assertSearchCurrent('foo\n|abc|\nbar\nabc\nmoo\nabc\nend')
+        self.feed('n_n')
+        self.assertSearch('foo\n|abc|\nbar\n|abc|\nmoo\n|abc|\nend')
+        self.assertSearchCurrent('foo\nabc\nbar\nabc\nmoo\n|abc|\nend')
 
-    def test_n_repeat_question_mark_partial(self):
+    def test_n_repeat_question_mark_should_only_match_whole_words(self):
         self.normal('|foo\nabc\nbar\nabcxmoo\nabc\nend')
         self.feed('n_?abc')
         self.feed('n_n')
