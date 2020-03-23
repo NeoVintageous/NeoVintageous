10b100cd236acf5c0fa5b138c78883db7d9478f4
Gerard Roche <gerardroche@users.noreply.github.com>
2020-03-23 00:40:13 +0000

    Refactor: mode (pass 1)

--- nv/settings.py
+++ nv/settings.py
@@ -147,6 +147,10 @@ def get_mode(view) -> str:
     return tmp_bc_get_session_view_value(view, 'mode', UNKNOWN)
 
 
+def set_mode(view, value: str) -> None:
+    tmp_bc_set_session_view_value(view, 'mode', value)
+
+
 # TODO [review] This seems to do the same thing as processing_notation.
 # TODO invert function name from is_non_interactive() -> is_interactive()
 def is_non_interactive(view) -> bool:
@@ -163,10 +167,6 @@ def is_non_interactive(view) -> bool:
 def set_non_interactive(view, value: bool) -> None:
     assert isinstance(value, bool), 'bool expected'  # TODO Remove assert statement
     set_session_view_value(view, 'non_interactive', value)
-
-
-def set_mode(view, value: str) -> None:
-    tmp_bc_set_session_view_value(view, 'mode', value)
 
 
 def is_processing_notation(view) -> bool:
