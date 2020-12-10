9a843c86f3d1d1bc7467742fccb745c02b2578c8
Gerard Roche <gerardroche@users.noreply.github.com>
2020-12-10 23:37:41 +0000

    Refactor #470

--- nv/events.py
+++ nv/events.py
@@ -124,23 +124,20 @@ class NeoVintageousEvents(EventListener):
         if key == 'nv_handle_key':
             handle_keys = get_setting(view, 'handle_keys')
             if handle_keys:
-
-                # Check if the key should be handled only for a specific mode.
-                # The format is "{mode}_{key}" e.g. "n_<C-w>", "v_<C-w>"
-                # meaning NORMAL, VISUAL respectively. No prefix implies all
-                # modes. See mode_to_char() for a list of valid mode prefixes.
-                cur_mode_char = mode_to_char(get_mode(view))
-                if cur_mode_char:
-                    try:
-                        return bool(handle_keys['%s_%s' % (cur_mode_char, operand)])
-                    except KeyError:
-                        pass
-
-                # Check if the key (no mode prefix; all modes) should be handled.
                 try:
+                    # Check if the key (no mode prefix; all modes) should be handled.
                     return bool(handle_keys[operand])
                 except KeyError:
-                    pass
+                    # Check if the key should be handled only for a specific mode.
+                    # The format is "{mode}_{key}" e.g. "n_<C-w>", "v_<C-w>"
+                    # meaning NORMAL, VISUAL respectively. No prefix implies all
+                    # modes. See mode_to_char() for a list of valid mode prefixes.
+                    cur_mode_char = mode_to_char(get_mode(view))
+                    if cur_mode_char:
+                        try:
+                            return bool(handle_keys['%s_%s' % (cur_mode_char, operand)])
+                        except KeyError:
+                            pass
 
             # By default all keys are handled.
             return True
