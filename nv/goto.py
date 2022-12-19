4494a1a325a31825f0c247a9ec394555c9d7d54b
Gerard Roche <gerardroche@users.noreply.github.com>
2022-12-19 15:49:02 +0000

    Disable highlight line in help

--- nv/goto.py
+++ nv/goto.py
@@ -178,6 +178,7 @@ def goto_help_subject(window, subject: str = None) -> None:
         settings.set('draw_centered', False)
         settings.set('draw_indent_guides', False)
         settings.set('draw_white_space', 'none')
+        settings.set('highlight_line', False)
         settings.set('line_numbers', False)
         settings.set('match_selection', False)
         settings.set('rulers', [])
