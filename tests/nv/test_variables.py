8ecae133ce22bcec8b1b7f0a0f10fcb71cee3bcd
Gerard Roche <gerardroche@users.noreply.github.com>
2025-12-16 20:21:32 +0000

    fix failing test
    re: https://github.com/NeoVintageous/NeoVintageous/actions/runs/19652732874/job/58244003367
    
    ERROR: NeoVintageous.tests.nv.test_variables (unittest.loader.ModuleImportFailure)
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "./python3.3/unittest/case.py", line 384, in _executeTestPart
      File "./python3.3/unittest/loader.py", line 32, in testFailure
    ImportError: Failed to import test module: NeoVintageous.tests.nv.test_variables
    Traceback (most recent call last):
      File "./python3.3/unittest/loader.py", line 261, in _find_tests
      File "./python3.3/unittest/loader.py", line 239, in _get_module_from_name
      File "/home/runner/.config/sublime-text-3/Packages/NeoVintageous/tests/nv/test_variables.py", line 29, in <module>
        class TestVariables(unittest.TestCase):
      File "/home/runner/.config/sublime-text-3/Packages/NeoVintageous/tests/nv/test_variables.py", line 41, in TestVariables
        @unittest.mock.patch.dict('NeoVintageous.nv.variables._special', {'<testpresent>': 'testpresentvalue'})
    AttributeError: 'module' object has no attribute 'mock'

--- tests/nv/test_variables.py
+++ tests/nv/test_variables.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with NeoVintageous.  If not, see <https://www.gnu.org/licenses/>.
 
-import unittest
+from NeoVintageous.tests import unittest
 
 from NeoVintageous.nv.variables import _defaults
 from NeoVintageous.nv.variables import _special
